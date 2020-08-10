e8f067afd1dcfa3f9dee3e5fc01cb6c5fb7a66d9
jaydeepkhatri <jaydeepkhatri79@gmail.com>
2020-08-10 15:48:55 +0530

    Removed the dot, it loaded the index page on every request

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('./', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
