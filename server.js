6d8c520fcafb90dd1db1755d7e70ac960b8567bd
jaydeepkhatri <jaydeepkhatri79@gmail.com>
2020-08-12 16:57:42 +0530

    ➕ Update to theme, added colors, added emoji, time

--- server.js
+++ server.js
@@ -25,17 +25,16 @@ io.sockets.on('connection', socket => {
     socket.on('new-user', name => {
         users[socket.id] = name;
         socket.broadcast.emit('user-connected', name);
-    })
+    });
     socket.on('send-chat-message', message => {
         socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-    })
+    });
     socket.on('disconnect', () => {
         socket.broadcast.emit('user-disconnected', users[socket.id])
         delete users[socket.id]
-    })
+    });
 
     socket.on('changetheme', (color) => {
         socket.broadcast.emit('themecolor', color)
-        // document.documentElement.style.cssText = color;
     });
 })
