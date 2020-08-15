a0345fab977531b344ca96a4a7405598171f0fb0
jaydeepkhatri <jaydeepkhatri79@gmail.com>
2020-08-15 13:12:19 +0530

    ❔ Public Folder test

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public');
+    res.sendFile(__dirname + 'public');
 });
 
 http.listen(port, function () {
